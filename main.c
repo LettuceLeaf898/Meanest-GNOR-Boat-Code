436e561b57165bc7630edd34aa5630917abed9e0
Erin <Erin.Weidel@ucf.edu>
2026-04-14 22:58:59 -0400

    Lap Code for Boat (V1)

--- main.c
+++ main.c
@@ -1,41 +1,94 @@
 #include <Servo.h>
+Servo rudder;
+Servo motor;
 
-Servo rudderServo; 
-Servo myESC;  // ESCs use the Servo library
+// Important Variables
+
+// Adjust this to adjust start-up delay
+#define Startup_Delay 5000 // Adjust time it takes for boat to start moving
+#define Calibration_Delay 2000 // Adjust time it takes for calibration
+
+// Adjust these to adjust rudder movement
+#define Rudder_Straight 90   
+#define Rudder_45_Degree_Left_Turn 60  // Needs to be adjusted based on actual turning movement
+
+// Adjust these to adjust timing
+#define Motor_On 1100 // Can be increased for faster boat, but also will eat up battery more
+#define Motor_Off 1000 // Can be used to stop ESC
+#define First_Boat_Lap_Segment_Time_Straight 10000 // Can adjust these values along with motor_on to mess with timing of lap segments
+#define Second_Boat_Lap_Segment_Time_Turn 5000
+#define Third_Boat_Lap_Segment_Time_Straight 3000
+#define Fourth_Boat_Lap_Segment_Time_Turn 5000
+#define Last_Boat_Lap_Segment_Time_Straight 10000
+
+// Defining Boat_Lap_Segment states (for switch case)
+enum Boat_Lap_Segment_States {
+  START,
+  First_Boat_Lap_Segment,
+  Second_Boat_Lap_Segment,
+  Third_Boat_Lap_Segment,
+  Fourth_Boat_Lap_Segment,
+  Last_Boat_Lap_Segment,
+  STOP
+};
+
+// Initializing Boat Lap Segment
+Boat_Lap_Segment_States Boat_Lap_Segment = START;
 
 void setup() {
-  // Initialize the SERVO
-  rudderServo.attach(9); 
-  rudderServo.write(0);
-  delay(50);
+  rudder.attach(9); // Tells program that Servo (Rudder) is attached to pin 9 on arduino (Data pin)
+  motor.attach(10); // Tells program that ESC (motor) is attached to pin 10 on arduino (Data pin)
 
-  // Initialize the ESC
-  myESC.attach(10); 
-  // --- STEP 1: ARMING ---
-  // Most ESCs need to hear 1000ms (Zero Throttle) to unlock.
-  myESC.writeMicroseconds(1000); 
-  // Wait 5-8 seconds for the ESC to finish its startup music
-  delay(8000); 
+  rudder.write(Rudder_Straight); // Initialized rudder to 0 degrees
+
+  // Calibrating ESC (motor)
+  motor.writeMicroseconds(2000); // Maximum for ESC
+  delay(2000);
+  motor.writeMicroseconds(1000); // Minimum for ESC
 
 }
 
 void loop() {
-    int x = 68; //Starting position for rudder servo
-    rudderServo.write(x); //change x to change the position of the rudder servo
-    delay(15); //delay for the servo to reach the position
-    x = 112; //Ending position for rudder servo
-    rudderServo.write(x);
-    delay(15);
-  
-  // We stay between 1000 (Off) and 1200 (Very Low Power)
-  for (int speed = 1000; speed <= 1100; speed += 5) {
-    myESC.writeMicroseconds(speed);
-    delay(100);
+  switch(Boat_Lap_Segment) {
+    case START:
+      // Start-Up Delay for Getting Boat Ready
+      delay(Startup_Delay - Calibration_Delay); 
+      Boat_Lap_Segment = First_Boat_Lap_Segment;
+      break;
+    case First_Boat_Lap_Segment:
+      motor.writeMicroseconds(Motor_On);
+      delay(First_Boat_Lap_Segment_Time_Straight);
+      Boat_Lap_Segment = Second_Boat_Lap_Segment;
+      break;
+    case Second_Boat_Lap_Segment:
+      motor.writeMicroseconds(Motor_On);
+      rudder.write(Rudder_45_Degree_Left_Turn);
+      delay(Second_Boat_Lap_Segment_Time_Turn);
+      Boat_Lap_Segment = Third_Boat_Lap_Segment;
+      break;
+    case Third_Boat_Lap_Segment:
+      motor.writeMicroseconds(Motor_On);
+      rudder.write(Rudder_Straight);
+      delay(Third_Boat_Lap_Segment_Time_Straight);
+      Boat_Lap_Segment = Fourth_Boat_Lap_Segment;
+      break;
+    case Fourth_Boat_Lap_Segment:
+      motor.writeMicroseconds(Motor_On);
+      rudder.write(Rudder_45_Degree_Left_Turn);
+      delay(Fourth_Boat_Lap_Segment_Time_Turn);
+      Boat_Lap_Segment = Last_Boat_Lap_Segment;
+      break;
+    case Last_Boat_Lap_Segment:
+      motor.writeMicroseconds(Motor_On);
+      rudder.write(Rudder_Straight);
+      delay(Last_Boat_Lap_Segment_Time_Straight);
+      Boat_Lap_Segment = STOP;
+      break;
+    case STOP:
+      motor.writeMicroseconds(Motor_Off);
+      rudder.write(Rudder_Straight);
+      break;
+
   }
 
-  delay(2000); // change 2000 to change how long the ESC runs at speed
-
-  // --- STEP 3: STOP ---
-  myESC.writeMicroseconds(1000); // change 1000 to change the speed at which the ESC stops (1000 is off)
-  return;
 }
